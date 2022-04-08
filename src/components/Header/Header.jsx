1721d9ba283db30235c68837e8544ddbeb1d7527
Taylor Noj <taylornoj@gmail.com>
2022-04-07 22:51:33 -0400

    add blur to nasa logo and create column

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,28 +4,37 @@ import { Container, Row, Col, Image } from 'react-bootstrap';
 import { detectDevice } from '../../utils.jsx';
 import './Header.scss';
 
-function Header() {
+function Header(props) {
+  const { visible, handleIconClick } = props
 
 
 
   return (
     <Container className="header-container">
       <Row>
-        
-          <Image
-            
-            src={nasaPic}
-            alt="logo"
-            className="app-logo"
-          />
-        
-          <Image
-            
-            src={apod}
-            alt="logo2"
-            className="app-logo2"
-          />
-        
+        <Col
+          xs={12}
+          md={4}
+          className={
+            !visible ? 'app-logo-container' : 'app-logo-container blur'
+          }
+        >
+
+        <Image
+
+        src={nasaPic}
+        alt="logo"
+        className="app-logo"
+        />
+        </Col>
+
+        <Image
+
+        src={apod}
+        alt="logo2"
+        className="app-logo2"
+        />
+
       </Row>
     </Container>
   )
